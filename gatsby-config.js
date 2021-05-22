54e8068f01ede824bdf4132303ad24a46ee72e2f
Josh Glazer <joshglazer@gmail.com>
2021-05-22 16:07:57 -0400

    removing sitemap plugin temporarily to get the application to build

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,13 +12,13 @@ module.exports = {
       },
     },
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
+    // "gatsby-plugin-sitemap",
+    // {
+    //   resolve: "gatsby-plugin-manifest",
+    //   options: {
+    //     icon: "src/images/icon.png",
+    //   },
+    // },
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
